202cd31ea6b721354f5e2f4896c84495518e3a49
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-21 17:16:52 +0200

    while index <...

--- cycle.py
+++ cycle.py
@@ -1,9 +1,13 @@
 word = input("Enter word: ")
 symbol = input("Enter symbol: ")
+whileIndex = int(input("While index < "))
 
 count = 0
-for i in word:
-    if i == symbol:
-        count += 1
+index = 0
+while index < whileIndex:
+    for i in word[index]:
+        if i == symbol:
+            count += 1
+    index += 1
 
 print("Count:", count)
